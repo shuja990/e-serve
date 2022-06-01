0defe2bb11dd3e199c6dd436fe309fb108163695
Shuja Ali <58367243+shuja990@users.noreply.github.com>
2022-06-02 01:22:25 +0500

    Update servicesRoutes.js

--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,27 +1,27 @@
 import express from 'express'
 const router = express.Router()
-import {
-  getServices,
-  getServiceById,
-  deleteService,
-  createService,
-  updateService,
-  createserviceReview,
-  getUserPosts,
-  deleteServiceAdmin
-} from '../controllers/serviceController.js'
+// import {
+//   getServices,
+//   getServiceById,
+//   deleteService,
+//   createService,
+//   updateService,
+//   createserviceReview,
+//   getUserPosts,
+//   deleteServiceAdmin
+// } from '../controllers/serviceController.js'
 import { protect,admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getServices).post(protect, createService)
-router.route('/:id/reviews').post(protect, createserviceReview)
-router
-  .route('/:id')
-  .get(getServiceById)
-  .delete(protect, deleteService)
-  .put(protect, updateService)
-router.route('/myposts/:id').get(protect, getUserPosts)
-router
-  .route('/admin/:id')
-  .delete(protect,admin,deleteServiceAdmin)
+// router.route('/').get(getServices).post(protect, createService)
+// router.route('/:id/reviews').post(protect, createserviceReview)
+// router
+//   .route('/:id')
+//   .get(getServiceById)
+//   .delete(protect, deleteService)
+//   .put(protect, updateService)
+// router.route('/myposts/:id').get(protect, getUserPosts)
+// router
+//   .route('/admin/:id')
+//   .delete(protect,admin,deleteServiceAdmin)
 
 export default router
